d5702074b905af78a03a5414bdd9d233f47ca0f1
Bela Arany <github@goabela.com>
2018-03-19 19:36:25 +0100

    Unit-testing has been added
    Added dev-deps:
    - Mocha
    - Chai
    
    Added unit-test:
    - Should be OK
    - Should throw InvalidKey Error

--- test/validator.js
+++ test/validator.js
@@ -1,76 +1,102 @@
+var assert = require("chai").assert
+var expect = require("chai").expect
 
 var schemaValidator = require("../index.js")
 
-var schema = {
-	username: {
-		type: "string"
-	},
-	name: {
-		children: {
-			last: {
-				type: "string"
-			},
-			first: {
-				type: "string"
-			},
-			nick: {
-				type: "string"
+
+describe("GOabela Schema Validator", () => {
+
+	/**
+	 * Defining the schema and the body.
+	 *
+	 * Later this will be modified for testing.
+	 */
+	let schema = {
+		username: {
+			type: "string"
+		},
+		name: {
+			children: {
+				last: {
+					type: "string"
+				},
+				first: {
+					type: "string"
+				},
+				nick: {
+					type: "string"
+				}
 			}
-		}
-	},
-	born: {
-		children: {
-			place: {
-				type: "string"
-			},
-			date: {
-				children: {
-					year: {
-						type: "number"
-					},
-					month: {
-						type: "string",
-						enum: ["Jan", "Feb", "Marc"]
-					},
-					day: {
-						type: "number"
+		},
+		born: {
+			children: {
+				place: {
+					type: "string"
+				},
+				date: {
+					children: {
+						year: {
+							type: "number"
+						},
+						month: {
+							type: "string",
+							enum: ["Jan", "Feb", "Marc"]
+						},
+						day: {
+							type: "number"
+						}
 					}
 				}
 			}
+		},
+		skills: {
+			type: "array"
 		}
-	},
-	skills: {
-		type: "array"
 	}
-}
 
-var body = {
-	username: "johndoe",
-	name: {
-		last: "Doe",
-		first: "John",
-		nick: "Johnny"
-	},
-	born: {
-		place: "Colorado, Denver",
-		date: {
-			year: 1985,
-			month: "Jan",
-			day: 15
-		}
-	},
-	skills: [
-		"SEO",
-		"HTML",
-		"CSS"
-	]
-}
+	let body = {
+		username: "johndoe",
+		name: {
+			last: "Doe",
+			first: "John",
+			nick: "Johnny"
+		},
+		born: {
+			place: "Colorado, Denver",
+			date: {
+				year: 1985,
+				month: "Jan",
+				day: 15
+			}
+		},
+		skills: [
+			"SEO",
+			"HTML",
+			"CSS"
+		]
+	}
 
-schemaValidator.validate(body, schema)
-	.then(() => {
-		console.log("No issue")
+	// Testing for success validation
+	it("Should be OK", () => {
+		return schemaValidator.validate(body, schema)
+		.then(() => {
+			assert
+		})
+		.catch(err => {
+			assert.fail(0, 1, "Validate thrown Error")
+		})
 	})
-	.catch(err => {
-		console.log("Total issues:")
-		console.log(JSON.stringify(err))
+
+	// Testing for `invalidKeys` error
+	it("Should throw InvalidKey Error", () => {
+		body.testingForInvalidKey = 123
+
+		return schemaValidator.validate(body, schema)
+		.then(() => {
+			assert.fail(0, 1, "Should have throwned an Error")
+		})
+		.catch(err => {
+			assert.property(err, "invalidKeys")
+		})
 	})
+})
