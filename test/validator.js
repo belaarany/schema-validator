977b936b39e8281c92ded1f10e12ad76a45d3f54
Bela Arany <github@goabela.com>
2018-03-19 20:18:06 +0100

    Before each method has been added to unit-testing

--- test/validator.js
+++ test/validator.js
@@ -9,72 +9,80 @@ describe("GOabela Schema Validator", () => {
 	/**
 	 * Defining the schema and the body.
 	 *
+	 * Resetting before each test.
 	 * Later this will be modified for testing.
 	 */
-	let schema = {
-		username: {
-			type: "string"
-		},
-		name: {
-			children: {
-				last: {
-					type: "string"
-				},
-				first: {
-					type: "string"
-				},
-				nick: {
-					type: "string"
+	let schema = null
+	let body = null
+
+	beforeEach(done => {
+		schema = {
+			username: {
+				type: "string"
+			},
+			name: {
+				children: {
+					last: {
+						type: "string"
+					},
+					first: {
+						type: "string"
+					},
+					nick: {
+						type: "string"
+					}
 				}
-			}
-		},
-		born: {
-			children: {
-				place: {
-					type: "string"
-				},
-				date: {
-					children: {
-						year: {
-							type: "number"
-						},
-						month: {
-							type: "string",
-							enum: ["Jan", "Feb", "Marc"]
-						},
-						day: {
-							type: "number"
+			},
+			born: {
+				children: {
+					place: {
+						type: "string"
+					},
+					date: {
+						children: {
+							year: {
+								type: "number"
+							},
+							month: {
+								type: "string",
+								enum: ["Jan", "Feb", "Marc"]
+							},
+							day: {
+								type: "number"
+							}
 						}
 					}
 				}
+			},
+			skills: {
+				type: "array"
 			}
-		},
-		skills: {
-			type: "array"
 		}
-	}
 
-	let body = {
-		username: "johndoe",
-		name: {
-			last: "Doe",
-			first: "John",
-			nick: "Johnny"
-		},
-		born: {
-			place: "Colorado, Denver",
-			date: {
-				year: 1985,
-				month: "Jan",
-				day: 15
-			}
-		},
-		skills: [
-			"SEO",
-			"HTML",
-			"CSS"
-		]
-	}
+		body = {
+			username: "johndoe",
+			name: {
+				last: "Doe",
+				first: "John",
+				nick: "Johnny"
+			},
+			born: {
+				place: "Colorado, Denver",
+				date: {
+					year: 1985,
+					month: "Jan",
+					day: 15
+				}
+			},
+			skills: [
+				"SEO",
+				"HTML",
+				"CSS"
+			]
+		}
+
+		done()
+	})
 
 	// Testing for success validation
 	it("Should be OK", () => {
@@ -89,6 +97,8 @@ describe("GOabela Schema Validator", () => {
 
 	// Testing for `invalidKeys` error
 	it("Should throw InvalidKey Error", () => {
+
+		// Adding an foreign key
 		body.testingForInvalidKey = 123
 
 		return schemaValidator.validate(body, schema)
