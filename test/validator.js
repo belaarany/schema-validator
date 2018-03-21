520eedce4d5f4c275230be6bb0914639fea8615d
Bela Arany <github@goabela.com>
2018-03-21 18:57:20 +0100

    Unit-testing: Invalid type test added
    Expect `string` instead of `number` test has been added.
    
    Test has been seperated to sections.

--- test/validator.js
+++ test/validator.js
@@ -86,57 +86,101 @@ describe("GOabela Schema Validator", () => {
 		done()
 	})
 
-	// Testing for success validation
-	it("Should be OK", () => {
-		return schemaValidator.validate(body, schema)
-		.then(() => {
-			assert
+	// Basic tests
+	describe("Basic tests", () => {
+
+		// TODO: empty body, empty schema
+
+		// Testing for success validation
+		it("Should be OK", () => {
+			return schemaValidator.validate(body, schema)
+			.then(() => {
+				assert
+			})
+			.catch(err => {
+				assert.fail(0, 1, "Validate thrown Error")
+			})
 		})
-		.catch(err => {
-			assert.fail(0, 1, "Validate thrown Error")
+
+		// Testing for `invalidKeys` error
+		it("Should throw InvalidKey Error", () => {
+
+			// Adding an foreign key
+			body.testingForInvalidKey1 = 123
+			body.testingForInvalidKey2 = { subKey2: null }
+			body.testingForInvalidKey3 = { subKey3: { subSubKey3: 123 } }
+			body.testingForInvalidKey4 = { subKey4: { subSubKey4: { subSubSubKey4: 123 } } }
+
+			return schemaValidator.validate(body, schema)
+			.then(() => {
+				assert.fail(0, 1, "Should have thrown an Error")
+			})
+			.catch(err => {
+				assert.property(err, "invalidKeys")
+				expect(err.invalidKeys).to.be.an.instanceof(Array)
+				expect(err.invalidKeys).to.have.lengthOf(4)
+
+				expect(err.invalidKeys[0]).to.equal("testingForInvalidKey1")
+				expect(err.invalidKeys[1]).to.equal("testingForInvalidKey2")
+				expect(err.invalidKeys[2]).to.equal("testingForInvalidKey3")
+				expect(err.invalidKeys[3]).to.equal("testingForInvalidKey4")
+			})
+		})
+
+		// Testing for `missingKey` error
+		it("Should throw MissingKey Error", () => {
+
+			// Adding extra keys to the schema
+			schema.requiredTest1 = { type: "string", required: true }
+			schema.requiredTest2 = { type: "string", required: false }
+			schema.requiredTest3 = { children: { subItem3: { type: "string", required: true } } }
+			schema.requiredTest4 = { children: { subItem4: { children: { subSubItem4: { type: "string", required: true } } } } }
+
+			return schemaValidator.validate(body, schema)
+			.then(() => {
+				assert.fail(0, 1, "Should have thrown an Error")
+			})
+			.catch(err => {
+				assert.property(err, "missingKeys")
+				expect(err.missingKeys).to.be.an.instanceof(Array)
+				expect(err.missingKeys).to.have.lengthOf(3)
+
+				expect(err.missingKeys[0]).to.equal("requiredTest1")
+				expect(err.missingKeys[1]).to.equal("requiredTest3.subItem3")
+				expect(err.missingKeys[2]).to.equal("requiredTest4.subItem4.subSubItem4")
+			})
 		})
 	})
 
-	// Testing for `invalidKeys` error
-	it("Should throw InvalidKey Error", () => {
+	// Type tests
+	describe("Type tests", () => {
 
-		// Adding an foreign key
-		body.testingForInvalidKey = 123
+		it("Should expect `string` instead of `number`", () => {
 
-		return schemaValidator.validate(body, schema)
-		.then(() => {
-			assert.fail(0, 1, "Should have thrown an Error")
-		})
-		.catch(err => {
-			assert.property(err, "invalidKeys")
-			expect(err.invalidKeys).to.be.an.instanceof(Array)
-			expect(err.invalidKeys).to.have.lengthOf(1)
+			// Setting up invalid types
+			body.invalidType1 = 123
+			schema.invalidType1 = { type: "string" }
+			body.invalidType2 = { subItem2: { subSubItem2: 47576458 } }
+			schema.invalidType2 = { children: { subItem2: { children: { subSubItem2: { type: "string" } } } } }
 
-			expect(err.invalidKeys[0]).to.equal("testingForInvalidKey")
-		})
-	})
+			return schemaValidator.validate(body, schema)
+			.then(() => {
+				assert.fail(0, 1, "Should have thrown an Error")
+			})
+			.catch(err => {
+				expect(err).to.have.property("invalidTypes")
 
-	// Testing for `missingKey` error
-	it("Should throw MissingKey Error", () => {
+				expect(err.invalidTypes).to.be.an.instanceof(Array)
+				expect(err.invalidTypes).to.have.lengthOf(2)
 
-		// Adding extra keys to the schema
-		schema.requiredTest1 = { type: "string", required: true }
-		schema.requiredTest2 = { type: "string", required: false }
-		schema.requiredTest3 = { children: { subItem3: { type: "string", required: true } } }
-		schema.requiredTest4 = { children: { subItem4: { children: { subSubItem4: { type: "string", required: true } } } } }
+				expect(err.invalidTypes[0].key).to.equal("invalidType1")
+				expect(err.invalidTypes[0].expected).to.equal("string")
+				expect(err.invalidTypes[0].received).to.equal("number")
 
-		return schemaValidator.validate(body, schema)
-		.then(() => {
-			assert.fail(0, 1, "Should have thrown an Error")
-		})
-		.catch(err => {
-			assert.property(err, "missingKeys")
-			expect(err.missingKeys).to.be.an.instanceof(Array)
-			expect(err.missingKeys).to.have.lengthOf(3)
-
-			expect(err.missingKeys[0]).to.equal("requiredTest1")
-			expect(err.missingKeys[1]).to.equal("requiredTest3.subItem3")
-			expect(err.missingKeys[2]).to.equal("requiredTest4.subItem4.subSubItem4")
+				expect(err.invalidTypes[1].key).to.equal("invalidType2.subItem2.subSubItem2")
+				expect(err.invalidTypes[1].expected).to.equal("string")
+				expect(err.invalidTypes[1].received).to.equal("number")
+			})
 		})
 	})
 })
